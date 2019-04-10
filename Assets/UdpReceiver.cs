af084b183db73644dad647dd9f943a6b6f7a7f5a
yuuuo <yuuto_ose@yahoo.co.jp>
2019-04-10 18:15:56 +0900

    Initial commit

---
+++ Assets/UdpReceiver.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UdpReceiver : MonoBehaviour {
+	public int listenPort = 2002; // ポートはサーバ・クライアントで合わせる必要がある
+	private bool received = false;
+
+    private struct UdpState {
+		public System.Net.IPEndPoint e;
+		public System.Net.Sockets.UdpClient u;
+	}
+
+	public void ReceiveCallback(System.IAsyncResult ar) {
+		System.Net.Sockets.UdpClient u = (System.Net.Sockets.UdpClient)((UdpState)(ar.AsyncState)).u;
+		System.Net.IPEndPoint e = (System.Net.IPEndPoint)((UdpState)(ar.AsyncState)).e;
+		var receiveBytes = u.EndReceive(ar, ref e);
+
+        int pos = 16; // 16バイトから開始
+
+        int width = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            width = width * 256 + receiveBytes[pos++];
+        }
+
+        int height = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            height = height * 256 + receiveBytes[pos++];
+        }
+
+        Texture2D texture = new Texture2D(width, height);
+        texture.LoadImage(receiveBytes);
+
+        RawImage image = GetComponent<RawImage>();
+        image.texture = texture;
+
+        Debug.Log("Received"); // ここに任意の処理を書く
+
+        received = true;
+	}
+
+	IEnumerator receive_loop() {
+		var e = new System.Net.IPEndPoint(System.Net.IPAddress.Any, listenPort);
+		var u = new System.Net.Sockets.UdpClient(e);
+		u.EnableBroadcast = true;
+		var s = new UdpState();
+		s.e = e;
+		s.u = u;
+		for (;;) {
+			received = false;
+			u.BeginReceive(new System.AsyncCallback(ReceiveCallback), s);
+			while (!received) {
+				yield return null;
+			}
+		}
+	}
+
+	void Start () {
+        StartCoroutine(receive_loop());
+	}
+}
---
+++ Assets/UdpSender.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UdpSender : MonoBehaviour {
+	public int listenPort = 2002; // ポートはサーバ・クライアントで合わせる必要がある
+	public Texture2D tex;
+	private static bool sent = false;
+	private Coroutine coroutine_ = null;
+
+	public static void SendCallback(System.IAsyncResult ar)
+	{
+		// System.Net.Sockets.UdpClient u = (System.Net.Sockets.UdpClient)ar.AsyncState;
+		sent = true;
+	}
+
+	IEnumerator send(string server, byte[] message)
+	{
+		Debug.Log("sending..");
+		var u = new System.Net.Sockets.UdpClient();
+		u.EnableBroadcast = true;
+		u.Connect(server, listenPort);
+        var sendBytes = message;
+		sent = false;
+		u.BeginSend(sendBytes, sendBytes.Length, 
+					new System.AsyncCallback(SendCallback), u);
+		while (!sent) {
+			yield return null;
+		}
+		u.Close();
+		coroutine_ = null;
+		Debug.Log("done.");
+	}
+
+	void Update() {
+		if (coroutine_ == null && Input.GetKeyDown(KeyCode.Space)) {
+            byte[] bytes = tex.EncodeToPNG();
+			string server = "127.0.0.1"; // ブロードキャストアドレスが使えるならそれがベスト。できなかったら相手のＩＰアドレスを直打ち
+			coroutine_ = StartCoroutine(send(server, bytes));
+			
+        }
+    }
+}
