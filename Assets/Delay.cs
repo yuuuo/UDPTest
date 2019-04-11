2509f2226c0b3297360631fa8164b2f9015485e0
yuuuo <yuuto_ose@yahoo.co.jp>
2019-04-11 18:14:58 +0900

    tekitou

---
+++ Assets/Delay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class TextureRecord
+{
+    public Texture2D texture2d;
+    public Time time;
+}
+
+public class Delay : MonoBehaviour
+{
+    public RenderTexture renderTexture;
+
+    private RawImage rawImage;
+    private Queue<TextureRecord> textureQueue;
+    
+    void Start()
+    {
+        rawImage = GetComponent<RawImage>();
+        textureQueue = new Queue<TextureRecord>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextureRecord textureRecord = new TextureRecord();
+        textureRecord.texture2d = new Texture2D(renderTexture.width, renderTexture.height);
+        RenderTexture currentActiveRT = RenderTexture.active;
+        RenderTexture.active = renderTexture;
+        textureRecord.texture2d.ReadPixels(new Rect(0, 0, textureRecord.texture2d.width, textureRecord.texture2d.height), 0, 0);
+        textureRecord.texture2d.Apply();
+        RenderTexture.active = currentActiveRT;
+
+        rawImage.texture = textureRecord.texture2d;
+    }
+}
--- Assets/Display.cs
+++ Assets/Display.cs
@@ -6,42 +6,30 @@ using System;
 public class Display : MonoBehaviour {
 
     public RenderTexture RenderTextureRef;
+    private Texture2D tex;
+    private Texture2D texture;
+    RawImage image;
 
     void Start () {
-        
+        tex = new Texture2D(RenderTextureRef.width, RenderTextureRef.height);
+        texture = new Texture2D(RenderTextureRef.width, RenderTextureRef.height);
+        image = GetComponent<RawImage>();
     }
 
-    private void Update()
+    void Update()
     {
         RenderTexture currentActiveRT = RenderTexture.active;
         RenderTexture.active = RenderTextureRef;
-        Texture2D tex = new Texture2D(RenderTextureRef.width, RenderTextureRef.height);
+
         tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
         tex.Apply();
         RenderTexture.active = currentActiveRT;
-        /*
-        byte[] receiveBytes = tex.EncodeToPNG();
-        int pos = 16; // 16バイトから開始
 
-        int width = 0;
-        for (int i = 0; i < 4; i++)
-        {
-            width = width * 256 + receiveBytes[pos++];
-        }
+        byte[] receiveBytes = tex.EncodeToJPG();
+        
 
-        int height = 0;
-        for (int i = 0; i < 4; i++)
-        {
-            height = height * 256 + receiveBytes[pos++];
-        }
-
-        Texture2D texture = new Texture2D(width, height);
         texture.LoadImage(receiveBytes);
-        */
-        RawImage image = GetComponent<RawImage>();
-        image.texture = tex;
-
-        Destroy(tex, 1);
-        //Destroy(texture, 1);
+        
+        image.texture = texture;
     }
 }
